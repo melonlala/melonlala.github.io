8eeff8fafa502d2a5cb385f7a4f9d9efe5665f58
melonlala <108521058+melonlala@users.noreply.github.com>
2026-06-14 04:15:48 +0000

    Deploying to gh-pages from @ melonlala/melonlala.github.io@e7ff7e5a58c33d29dd017e05c919d5a814b24632 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -511,14 +511,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%6F%75.%79%75%63%68%65%6E@%6E%6F%72%74%68%65%61%73%74%65%72%6E.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -539,7 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=PNBSs3cAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
